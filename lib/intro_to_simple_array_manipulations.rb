2376e18f24957a4f6a27ac1fe07bbcee418dde09
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-18 19:41:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array)
-  array.delete_at()
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
   
